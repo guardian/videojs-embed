0cb65b7312f0e6c4a6fc5a1f976f73ef3ef2d734
Richard Nguyen <richard.nguyen@guardian.co.uk>
2015-10-07 10:10:41 +0100

    Upgrade to videojs 5.0.0

--- videojs.embed.js
+++ videojs.embed.js
@@ -23,37 +23,36 @@
 
         if (!options.embeddable) { return false; }
 
-        var player = this;
+        var player = this,
+            vjsComponent = videojs.getComponent('Component'),
+            vjsButton = videojs.getComponent('Button');
 
-        videojs.EmbedOverlayInput = videojs.Component.extend({
-            init: function(player, options) {
-                videojs.Component.call(this, player, options);
+        videojs.EmbedOverlayInput = videojs.extend(vjsComponent, {
+            constructor: function(player, options) {
+                vjsComponent.call(this, player, options);
                 this.location = options.location;
             }
         });
 
-        videojs.EmbedOverlayInput.prototype.contentEl = function() { return this.el_; };
-        videojs.EmbedOverlayInput.prototype.el = function() {
-            return this.el_ = videojs.Component.prototype.createEl('input', {
-                className: 'vjs-embedoverlay-input',
-                type: 'text'
-            });
-        };
-
         videojs.EmbedOverlayInput.prototype.setSrc = function() {
-            this.contentEl().value = '<iframe src="' + this.location + '" width="560" height="315" frameborder="0" allowfullscreen></iframe>';
+            this.el().value = '<iframe src="' + this.location + '" width="560" height="315" frameborder="0" allowfullscreen></iframe>';
         };
 
-        videojs.EmbedOverlay = videojs.Component.extend({
-            init: function(player, options) {
-                videojs.Component.call(this, player, options);
+        videojs.EmbedOverlay = videojs.extend(vjsComponent, {
+            constructor: function(player, options) {
+                vjsComponent.call(this, player, options);
 
                 this.input = new videojs.EmbedOverlayInput(player, {
-                    location: options.location
+                    location: options.location,
+                    el: vjsComponent.prototype.createEl('input', {
+                        className: 'vjs-embedoverlay-input'
+                    },{
+                        type: 'text'
+                    })
                 });
                 this.hide();
                 player.addChild(this);
-                this.contentEl().appendChild(this.input.el());
+                this.el().appendChild(this.input.el());
                 this.input.setSrc();
             }
         });
@@ -62,29 +61,25 @@
         videojs.EmbedOverlay.prototype.show = function() {
             this.el().style.display = 'block';
             this.isVisible = true;
-            this.input.contentEl().focus();
-            this.input.contentEl().select();
+            this.input.el().focus();
+            this.input.el().select();
         };
         videojs.EmbedOverlay.prototype.hide = function() { this.el().style.display = 'none'; this.isVisible = false; };
-        videojs.EmbedOverlay.prototype.contentEl = function() { return this.el_; };
-
-        videojs.EmbedOverlay.prototype.el = function() {
-            return this.el_ = this.el_ || videojs.Component.prototype.createEl(null, {
-                className: 'vjs-embedoverlay',
-                innerHTML: '<div class="vjs-embedoverlay-content"><span class="vjs-embedoverlay-text">Embed code</span></div>'
-            })
-        };
 
         videojs.EmbedOverlay.prototype.toggle = function() {
             this[this.isVisible ? 'hide' : 'show']();
         };
 
-        videojs.EmbedButton = videojs.Button.extend({
-            init: function (player, options) {
-                videojs.Button.call(this, player, options);
+        videojs.EmbedButton = videojs.extend(vjsButton, {
+            constructor: function (player, options) {
+                vjsButton.call(this, player, options);
                 this.on('click', this.onClick);
                 this.overlay = new videojs.EmbedOverlay(player, {
-                    location: options.location
+                    location: options.location,
+                    el: vjsComponent.prototype.createEl('div', {
+                        className: 'vjs-embedoverlay',
+                        innerHTML: '<div class="vjs-embedoverlay-content"><span class="vjs-embedoverlay-text">Embed code</span></div>'
+                    })
                 });
             }
         });
@@ -96,9 +91,10 @@
         player.ready(function() {
             var button = new videojs.EmbedButton(player, {
                 location: options.location,
-                el: videojs.Component.prototype.createEl(null, {
+                el: vjsComponent.prototype.createEl('div', {
                     className: 'vjs-embed-button vjs-control',
-                    innerHTML: '<div class="vjs-control-content"><span class="vjs-control-text">Embed</span></div>',
+                    innerHTML: '<div class="vjs-control-content"><span class="vjs-control-text">Embed</span></div>'
+                },{
                     role: 'button'
                 })
             });
